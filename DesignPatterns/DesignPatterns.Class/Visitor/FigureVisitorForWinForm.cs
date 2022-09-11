0873f37c2ac3d6693d510ba41bea181a531297d4
Turleque Fabrice <57303912+FTurleque@users.noreply.github.com>
2022-09-11 13:34:11 +0200

    Started design pattern visitor

---
+++ DesignPatterns/DesignPatterns.Class/Visitor/FigureVisitorForWinForm.cs
@@ -0,0 +1,33 @@
+﻿using DesignPatterns.Class.Composite.Draw;
+using DesignPatterns.Class.Composite.Draw.Figures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Class.Visitor
+{
+    public class FigureVisitorForWinForm : IFigureVisitor
+    {
+        public void Visit(Rectangle rectangle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Visit(Circle circle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Visit(Triangle triangle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Visit(FigureS figureS)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatterns.Class/Visitor/IFigureVisitor.cs
@@ -0,0 +1,17 @@
+﻿using DesignPatterns.Class.Composite.Draw;
+using DesignPatterns.Class.Composite.Draw.Figures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DesignPatterns.Class.Visitor
+{
+    public interface IFigureVisitor
+    {
+        public void Visit(Rectangle rectangle);
+        public void Visit(Circle circle);
+        public void Visit(Triangle triangle);
+        public void Visit(FigureS figureS);
+    }
+}
